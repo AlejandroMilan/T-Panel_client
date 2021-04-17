0fb30e7c5aa1f9a1ece5c8b02e91b1a13b28db15
Alejandro Milán <alex.13062001des@gmail.com>
2021-04-17 16:04:05 -0500

    User conditional for app components added

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <v-app>
-    <v-navigation-drawer app> </v-navigation-drawer>
-    <v-app-bar app> </v-app-bar>
+    <v-navigation-drawer v-if="user" app></v-navigation-drawer>
+    <v-app-bar v-if="user" app></v-app-bar>
     <v-main>
       <v-container fluid>
         <router-view></router-view>
       </v-container>
     </v-main>
-    <v-footer app> </v-footer>
+    <v-footer v-if="user" app> </v-footer>
   </v-app>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   name: "App",
+
+  computed: {
+    ...mapGetters(["user"]),
+  },
+
+  mounted() {
+    console.log(this.user);
+  },
 };
 </script>
