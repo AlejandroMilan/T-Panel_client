531312a32b6389fdc08281e0c6cc884dc4eaa2ab
Alejandro Milán <alex.13062001des@gmail.com>
2021-04-19 02:20:41 -0500

    Login view built

--- src/components/login/login.view.vue
+++ src/components/login/login.view.vue
@@ -1,11 +1,35 @@
 <template>
-  <div>
-    <p>Hola mundo</p>
-  </div>
+  <v-row align="center">
+    <v-col cols="12" md="6">
+      <div id="banner">
+        <v-card
+          elevation="0"
+          class="banner-card"
+          :height="$vuetify.breakpoint.mdAndUp ? '96vh' : '30vh'"
+          color="primary"
+        ></v-card>
+      </div>
+    </v-col>
+    <v-col cols="12" md="6">
+      <div class="form">
+        <loginForm></loginForm>
+      </div>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
+import loginForm from "./loginForm";
+
 export default {
   name: "loginView",
+
+  components: {
+    loginForm,
+  },
+
+  mounted() {
+    console.log(this.$vuetify.breakpoint);
+  },
 };
 </script>
