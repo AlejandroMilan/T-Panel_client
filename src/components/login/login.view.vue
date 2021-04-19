3e4e8bccd50a1baa1f1c739ec613010e34470711
Alejandro Milán <alex.13062001des@gmail.com>
2021-04-19 12:27:42 -0500

    Login welcome created

--- src/components/login/login.view.vue
+++ src/components/login/login.view.vue
@@ -11,6 +11,14 @@
       </div>
     </v-col>
     <v-col cols="12" md="6">
+      <div class="welcome my-2">
+        <div>
+          <span class="text-h3">Bienvenido</span>
+        </div>
+        <span class="text-body-1"
+          >Para continuar, inicia sesión con tu cuenta o crea una nueva.</span
+        >
+      </div>
       <div class="form">
         <loginForm></loginForm>
       </div>
