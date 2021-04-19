faa9a6d229ded3184c4cb2e4cd868fe5c4e7949c
Alejandro Milán <alex.13062001des@gmail.com>
2021-04-19 02:20:52 -0500

    Login form built

---
+++ src/components/login/loginForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-form>
+      <v-row no-gutters>
+        <v-col cols="12">
+          <v-text-field
+            v-model="email"
+            label="Correo electrónico"
+            outlined
+            color="primary"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12">
+          <v-text-field
+            v-model="password"
+            outlined
+            label="Contraseña"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="showPassword ? 'text' : 'password'"
+            @click:append="showPassword = !showPassword"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12">
+          <v-btn color="primary" right>Iniciar sesión</v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "loginForm",
+
+  data: () => ({
+    showPassword: false,
+    email: "",
+    password: "",
+  }),
+};
+</script>
