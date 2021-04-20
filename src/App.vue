0f6eb5edd3cd5cac358e36020b94e2996774d43a
Alejandro Milán <alex.13062001des@gmail.com>
2021-04-20 17:17:13 -0500

    Session load on app before mount

--- src/App.vue
+++ src/App.vue
@@ -14,15 +14,36 @@
 <script>
 import { mapGetters } from "vuex";
 
+import { validateSession } from "./auth.service";
+
 export default {
   name: "App",
 
   computed: {
-    ...mapGetters(["user"]),
+    ...mapGetters(["user", "httpUrl"]),
   },
 
-  mounted() {
-    console.log(this.user);
+  async beforeMount() {
+    const sessionToken = localStorage.getItem("sessionToken");
+    if (sessionToken) {
+      try {
+        const response = await validateSession(this.httpUrl, sessionToken);
+        this.$store.dispatch("setUser", response.data.user);
+        this.$store.dispatch("setSessionToken", sessionToken);
+        if (this.$route.path === "/login")
+          this.$router.push("/panel")
+      } catch (error) {
+        if (error.response.status >= 500) console.error(error);
+        else {
+          console.error(
+            "Error al cargar la sesión actual, usuario no encontrado o sesión caducada"
+          );
+          localStorage.removeItem("sessionToken");
+          if (this.$route.path !== "/login")
+            this.$router.push("/login")
+        }
+      }
+    }
   },
 };
 </script>
