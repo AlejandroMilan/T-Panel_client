6c4463345bc0c958bb2f896b7820ae42a594aa6b
Alejandro Milán <alex.13062001des@gmail.com>
2021-04-19 19:50:57 -0500

    Env variables changed to vuex

--- src/store/index.js
+++ src/store/index.js
@@ -4,14 +4,22 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 const initialState = {
+  enviroment: process.env.VUE_APP_ENVIROMENT,
+  httpUrl: function () {
+    return this.enviroment == "dev" ? process.env.VUE_APP_HTTP_URL_DEV : ""
+  },
   user: null,
 };
+
 
 export default new Vuex.Store({
   state: initialState,
   getters: {
     user: (state) => {
       return state.user;
+    },
+    httpUrl: (state) => {
+      return state.httpUrl()
     },
   },
   mutations: {},
