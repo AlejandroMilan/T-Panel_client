0494e1a46bc8dae4313174c5f22b8fb4f9eb57e3
Alejandro Milán <alex.13062001des@gmail.com>
2021-04-21 17:41:53 -0500

    Session validation removed of App component

--- src/App.vue
+++ src/App.vue
@@ -14,34 +14,15 @@
 <script>
 import { mapGetters } from "vuex";
 
-import { validateSession } from "./auth.service";
+import navigationDrawer from "./components/layout/navigationDrawer";
 
 export default {
   name: "App",
 
+  components: { navigationDrawer },
+
   computed: {
     ...mapGetters(["user", "httpUrl"]),
-  },
-
-  async beforeMount() {
-    const sessionToken = localStorage.getItem("sessionToken");
-    if (sessionToken) {
-      try {
-        const response = await validateSession(this.httpUrl, sessionToken);
-        this.$store.dispatch("setUser", response.data.user);
-        this.$store.dispatch("setSessionToken", sessionToken);
-        if (this.$route.path === "/login") this.$router.replace("/panel");
-      } catch (error) {
-        if (error.response.status >= 500) console.error(error);
-        else {
-          console.error(
-            "Error al cargar la sesión actual, usuario no encontrado o sesión caducada"
-          );
-          localStorage.removeItem("sessionToken");
-          if (this.$route.path !== "/login") this.$router.replace("/login");
-        }
-      }
-    }
   },
 };
 </script>
