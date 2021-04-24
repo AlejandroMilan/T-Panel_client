4372f9498cfa54691bb5ab56e53397a40969ba89
Alejandro Milán <alex.13062001des@gmail.com>
2021-04-24 17:08:39 -0500

    Business data component mockup and imported to business view

--- src/components/business/business.view.vue
+++ src/components/business/business.view.vue
@@ -1,17 +1,24 @@
 <template>
   <div>
     <hasNoBusiness></hasNoBusiness>
+    <v-row dense class="my-5">
+      <v-col cols="12" md="6">
+        <businessData></businessData>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 import hasNoBusiness from "./hasNoBusiness";
+import businessData from "./businessData";
 
 export default {
   name: "businessView",
 
   components: {
     hasNoBusiness,
+    businessData,
   },
 };
 </script>
---
+++ src/components/business/businessData.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card>
+    <div v-for="(section, index) in sections" :key="index">
+      <v-subheader>{{ section.sectionName }}</v-subheader>
+      <v-row dense>
+        <v-col
+          cols="12"
+          md="6"
+          v-for="(field, index) in section.fields"
+          :key="index"
+        >
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>{{ field.fieldName }}</v-list-item-title>
+              <v-list-item-subtitle>{{
+                field.fieldValue
+              }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "businessData",
+
+  data: () => ({
+    sections: {
+      generalData: {
+        sectionName: "Datos generales",
+        fields: [],
+      },
+      contactData: {
+        sectionName: "Datos de contacto",
+        fields: [],
+      },
+      adressData: {
+        sectionName: "Domicilio",
+        fields: [],
+      },
+    },
+  }),
+};
+</script>
