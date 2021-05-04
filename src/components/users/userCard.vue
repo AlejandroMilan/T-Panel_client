0582bad1310e13ebea04603f6f76449515a6812a
Alejandro Milán <alex.13062001des@gmail.com>
2021-05-04 13:50:12 -0500

    User card mockup completed

--- src/components/users/userCard.vue
+++ src/components/users/userCard.vue
@@ -1,11 +1,30 @@
 <template>
   <v-card outlined>
     <v-card-title>
-      <span> Alejandro Milán</span>
+      <a href="javascript:void(0)" class="text-decoration-none"
+        >Alejandro Milán</a
+      >
       <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
+      <v-menu bottom left>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list dense>
+          <v-list-item-group>
+            <v-list-item v-for="(option, index) in options" :key="index">
+              <v-list-item-icon
+                ><v-icon :color="option.color">{{
+                  option.icon
+                }}</v-icon></v-list-item-icon
+              >
+              <v-list-item-title>{{ option.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-menu>
     </v-card-title>
     <v-card-subtitle> Administrador </v-card-subtitle>
   </v-card>
@@ -14,5 +33,20 @@
 <script>
 export default {
   name: "userCard",
+
+  data: () => ({
+    options: [
+      {
+        title: "Modificar",
+        color: "primary",
+        icon: "mdi-pencil",
+      },
+      {
+        title: "Eliminar",
+        color: "error",
+        icon: "mdi-delete",
+      },
+    ],
+  }),
 };
 </script>
