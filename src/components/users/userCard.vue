5b24fdea9ae57fdd7d14a15f8e4d1dc101cccd70
Alejandro Milán <alex.13062001des@gmail.com>
2021-05-04 13:19:35 -0500

    User card mockup started

---
+++ src/components/users/userCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card outlined>
+    <v-card-title>
+      <span> Alejandro Milán</span>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-card-title>
+    <v-card-subtitle> Administrador </v-card-subtitle>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "userCard",
+};
+</script>
--- src/components/users/users.view.vue
+++ src/components/users/users.view.vue
@@ -1,9 +1,19 @@
 <template>
-  <h1>Users</h1>
+  <div>
+    <v-row dense>
+      <v-col cols="12" md="6" v-for="i in 4" :key="i">
+        <userCard class="mb-2"></userCard>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
+import userCard from "./userCard";
+
 export default {
   name: "usersView",
+
+  components: { userCard },
 };
 </script>
