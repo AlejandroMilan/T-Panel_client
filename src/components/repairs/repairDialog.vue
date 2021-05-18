62dbe46938aaf9c15b2f5dca4a9265ca50710b26
Alejandro Milán <alex.13062001des@gmail.com>
2021-05-18 15:11:47 -0500

    Rapri dialog created and imported to repair view

---
+++ src/components/repairs/repairDialog.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-dialog
+      persistent
+      v-model="show"
+      :fullscreen="!$vuetify.breakpoint.mdAndUp"
+      :width="$vuetify.breakpoint.mdAndUp ? '750' : null"
+    >
+      <v-card>
+        <v-card-title>Agregar reparación</v-card-title>
+        <v-card-text>
+          <v-stepper v-model="formStep" vertical class="elevation-0">
+            <v-stepper-step step="1">Datos del dispositivo</v-stepper-step>
+            <v-stepper-step step="2">Datos del cliente</v-stepper-step>
+            <v-stepper-step step="3">
+                Otros datos
+                <small>Folio del servicio, costos</small>
+            </v-stepper-step>
+          </v-stepper>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "repairDialog",
+
+  props: {
+    show: { type: Boolean, defualt: false },
+  },
+
+  data: () => ({
+    formStep: 1,
+  }),
+};
+</script>
--- src/components/repairs/repairs.view.vue
+++ src/components/repairs/repairs.view.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
-    <h1>Repairs</h1>
+    <v-progress-linear
+      indeterminate
+      color="primary"
+      v-if="loading"
+    ></v-progress-linear>
+    <v-row v-else dense>
+      <v-col cols="12">
+        <div class="d-flex justify-end">
+          <v-btn color="primary" @click="showRepairDialog = true">
+            <v-icon>mdi-plus</v-icon>
+            Nueva reparación</v-btn
+          >
+          <repairDialog :show="showRepairDialog"></repairDialog>
+        </div>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
+import repairDialog from "./repairDialog";
+
 export default {
   name: "repairsView",
+
+  components: {
+    repairDialog,
+  },
+
+  data: () => ({
+    loading: false,
+    showRepairDialog: false,
+  }),
 };
 </script>
