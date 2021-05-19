94b1670d24ceca1c9875950043c9613bc641cc44
Alejandro Milán <alex.13062001des@gmail.com>
2021-05-19 17:55:37 -0500

    "General data" step validators added

--- src/components/repairs/generalDataDialogStep.vue
+++ src/components/repairs/generalDataDialogStep.vue
@@ -31,27 +31,27 @@
       </v-col>
       <v-col cols="12" md="6">
         <v-text-field
-          v-model="invoiceId"
+          v-model="payment.estimatedCost"
           label="Costo estimado (opcional)"
           outlined
           dense
           color="primary"
-          :error-messages="errors.invoiceId"
-          @input="validateInvoiceId()"
-          @blur="validateInvoiceId()"
+          :error-messages="errors.estimatedCost"
+          @input="validateEstimatedCost()"
+          @blur="validateEstimatedCost()"
         >
         </v-text-field>
       </v-col>
       <v-col cols="12" md="6">
         <v-text-field
-          v-model="invoiceId"
+          v-model="payment.prePayment"
           label="Adelanto del cliente (opcional)"
           outlined
           dense
           color="primary"
-          :error-messages="errors.invoiceId"
-          @input="validateInvoiceId()"
-          @blur="validateInvoiceId()"
+          :error-messages="errors.prePayment"
+          @input="validatePrepayment()"
+          @blur="validatePrepayment()"
         >
         </v-text-field>
       </v-col>
@@ -64,7 +64,9 @@
             class="mr-2"
             >Paso anterior</v-btn
           >
-          <v-btn color="primary">Guardar reparación</v-btn>
+          <v-btn color="primary" :disabled="!isFormValid"
+            >Guardar reparación</v-btn
+          >
         </div>
       </v-col>
     </v-row>
@@ -72,19 +74,67 @@
 </template>
 
 <script>
+import { validationMixin } from "vuelidate";
+import { required, numeric } from "vuelidate/lib/validators";
+
 export default {
   name: "generalDataDialogStep",
 
+  mixins: [validationMixin],
+
+  computed: {
+    isFormValid() {
+      if (this.errors.invoiceId.length) return false;
+      if (this.errors.estimatedCost.length) return false;
+      if (this.errors.prePayment.length) return false;
+      return true;
+    },
+  },
+
   data: () => ({
     invoiceId: "",
+    payment: {
+      estimatedCost: "",
+      prePayment: "",
+    },
     errors: {
       invoiceId: [],
+      estimatedCost: [],
+      prePayment: [],
     },
   }),
 
+  validations: {
+    invoiceId: { required },
+    payment: {
+      estimatedCost: { numeric },
+      prePayment: { numeric },
+    },
+  },
+
   methods: {
     validateInvoiceId() {
-      console.log(this.invoiceId);
+      const errors = [];
+      this.$v.invoiceId.$touch();
+      !this.$v.invoiceId.required &&
+        errors.push("El número de folio es requerido");
+      this.errors.invoiceId = errors;
+    },
+
+    validateEstimatedCost() {
+      const errors = [];
+      this.$v.payment.estimatedCost.$touch();
+      !this.$v.payment.estimatedCost.numeric &&
+        errors.push("Ingresa una cantidad válida");
+      this.errors.estimatedCost = errors;
+    },
+
+    validatePrepayment() {
+      const errors = [];
+      this.$v.payment.prePayment.$touch();
+      !this.$v.payment.prePayment.numeric &&
+        errors.push("Ingresa una cantidad válida");
+      this.errors.prePayment = errors;
     },
   },
 };
