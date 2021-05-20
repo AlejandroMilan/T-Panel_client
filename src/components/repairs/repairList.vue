916b773b7f92e0d921ca246e702950bf27b95d98
Alejandro Milán <alex.13062001des@gmail.com>
2021-05-20 13:08:18 -0500

    Repair list working with repairs data

--- src/components/repairs/repairList.vue
+++ src/components/repairs/repairList.vue
@@ -1,9 +1,60 @@
 <template>
-  <div>repairs</div>
+  <div class="py-3">
+    <v-card outlined>
+      <v-card-title>
+        <span>Últimas reparaciones</span>
+        <v-spacer> </v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Buscar por folio, marca, modelo, cliente o estado"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-card-text>
+        <v-data-table
+          :headers="headers"
+          :items="repairs"
+          :search="search"
+        ></v-data-table>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
 export default {
   name: "repairList",
+
+  props: {
+    repairs: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  data: () => ({
+    search: "",
+    headers: [
+      { text: "Folio", value: "invoiceId" },
+      {
+        text: "marca",
+        value: "device.trademark",
+      },
+      {
+        text: "modelo",
+        value: "device.model",
+      },
+      {
+        text: "Cliente",
+        value: "customer.name",
+      },
+      {
+        text: "Estado",
+        value: "status.title",
+      },
+    ],
+  }),
 };
 </script>
