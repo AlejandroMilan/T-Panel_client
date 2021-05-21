4873186f90ac86234960ae2aeb095474b0197343
Alejandro Milán <alex.13062001des@gmail.com>
2021-05-21 13:45:39 -0500

    Repair view getter working and error message works

--- src/components/repairs/repair.view.vue
+++ src/components/repairs/repair.view.vue
@@ -1,13 +1,58 @@
 <template>
-  <div>repair</div>
+  <div>
+    <v-progress-linear
+      v-if="loading"
+      indeterminate
+      color="primary"
+    ></v-progress-linear>
+    <v-alert type="error" v-if="!loading && error">
+      <v-row align="center">
+        <v-col class="grow"> {{ error }} </v-col>
+        <v-col class="shrink">
+          <v-btn color="white" outlined
+            >Volver a "Todas las reparaciones"</v-btn
+          >
+        </v-col>
+      </v-row>
+    </v-alert>
+  </div>
 </template>
 
 <script>
+import serverRequestMixin from "@/mixins/serverRequest.mixin";
+
 export default {
   name: "RepairView",
 
+  mixins: [serverRequestMixin],
+
+  data: () => ({
+    loading: false,
+    error: null,
+    repairId: "",
+    repair: null,
+  }),
+
   mounted() {
-    console.log(this.$route.params.repairId);
+    this.repairId = this.$route.params.repairId;
+    this.getRepairInfo();
+  },
+
+  methods: {
+    async getRepairInfo() {
+      this.loading = true;
+      try {
+        const response = await this.getRequest(
+          `/repairs/repair/${this.repairId}`
+        );
+        this.loading = false;
+        console.log(response);
+      } catch (error) {
+        this.loading = false;
+        this.error = error.data.message;
+        if (error.status >= 500) console.error(error);
+      }
+    },
   },
 };
 </script>
