c81aba16815ccb1003ebc66f814e718b8d4e7dbe
Alejandro Milán <alex.13062001des@gmail.com>
2021-05-30 16:47:06 -0500

    Buttons of actions card changed to text style

--- src/components/repairs/actionsCard.vue
+++ src/components/repairs/actionsCard.vue
@@ -5,25 +5,25 @@
       <v-card-text>
         <v-row dense>
           <v-col cols="12">
-            <v-btn block color="primary" small @click="$emit('editRepairInfo')"
+            <v-btn color="primary" small @click="$emit('editRepairInfo')" text
               ><v-icon small class="mr-2"> mdi-pencil</v-icon>Editar
               información</v-btn
             >
           </v-col>
           <v-col cols="12">
-            <v-btn block color="accent" small
+            <v-btn color="accent" small text
               ><v-icon small class="mr-2"> mdi-devices</v-icon>Cambiar
               estado</v-btn
             >
           </v-col>
           <v-col cols="12">
-            <v-btn block color="secondary" small
+            <v-btn color="secondary" small text
               ><v-icon small class="mr-2"> mdi-message-reply-text</v-icon>Nuevo
               comentario</v-btn
             >
           </v-col>
           <v-col cols="12">
-            <v-btn block color="error darken-2" small
+            <v-btn color="error darken-2" small text
               ><v-icon small class="mr-2"> mdi-delete</v-icon>Eliminar
               reparación</v-btn
             >
