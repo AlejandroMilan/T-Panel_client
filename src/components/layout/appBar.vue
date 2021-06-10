83b0467a5c51e600cc9a85f1221dca55d553bc62
Alejandro Milán <alex.13062001des@gmail.com>
2021-06-10 15:36:54 -0500

    Logout action working

--- src/components/layout/appBar.vue
+++ src/components/layout/appBar.vue
@@ -1,9 +1,27 @@
 <template>
-  <v-app-bar app></v-app-bar>
+  <v-app-bar app>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-toolbar-title>{{ $route.name }}</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon @click="exit">
+      <v-icon>mdi-exit-to-app</v-icon>
+    </v-btn>
+  </v-app-bar>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   name: "appBar",
+
+  methods: {
+    ...mapActions(["logOut"]),
+
+    exit() {
+      this.logOut();
+      this.$router.push({ path: "/login" });
+    },
+  },
 };
 </script>
