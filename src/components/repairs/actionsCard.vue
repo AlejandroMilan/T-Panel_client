2b3805217753225850843d1c81cd5ff2fe262a53
Alejandro Milán <alex.13062001des@gmail.com>
2021-06-11 13:09:35 -0500

    Permissions validators added to repair views

--- src/components/repairs/actionsCard.vue
+++ src/components/repairs/actionsCard.vue
@@ -4,13 +4,25 @@
       <v-card-title>Acciones</v-card-title>
       <v-card-text>
         <v-row dense>
-          <v-col cols="12">
+          <v-col
+            cols="12"
+            v-if="
+              user.role.role === 0 ||
+              user.permissions.filter((e) => e.key === 330).length > 0
+            "
+          >
             <v-btn color="primary" small @click="$emit('editRepairInfo')" text
               ><v-icon small class="mr-2"> mdi-pencil</v-icon>Editar
               información</v-btn
             >
           </v-col>
-          <v-col cols="12">
+          <v-col
+            cols="12"
+            v-if="
+              user.role.role === 0 ||
+              user.permissions.filter((e) => e.key === 331).length > 0
+            "
+          >
             <v-btn color="accent" small text @click="$emit('editRepairStatus')"
               ><v-icon small class="mr-2"> mdi-devices</v-icon>Cambiar
               estado</v-btn
@@ -22,7 +34,13 @@
               comentario</v-btn
             >
           </v-col>
-          <v-col cols="12">
+          <v-col
+            cols="12"
+            v-if="
+              user.role.role === 0 ||
+              user.permissions.filter((e) => e.key === 340).length > 0
+            "
+          >
             <v-btn
               color="error darken-2"
               small
@@ -39,7 +57,13 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
   name: "actionsCard",
+
+  computed: {
+    ...mapGetters(["user"]),
+  },
 };
 </script>
--- src/components/repairs/repairs.view.vue
+++ src/components/repairs/repairs.view.vue
@@ -15,6 +15,11 @@
             {{ $vuetify.breakpoint.mdAndUp ? "Actualizar" : null }}</v-btn
           >
           <v-btn
+            v-if="
+              user.role.role === 0 ||
+              user.permissions.filter((permission) => permission.key === 320)
+                .length > 0
+            "
             :disabled="loading"
             :loading="loading"
             color="primary"
@@ -34,11 +39,15 @@
       <v-col cols="12">
         <repairList :repairs="repairs" :loading="loading"></repairList>
       </v-col>
+      <v-col v-if="error" cols="12">
+        <v-alert type="error" outlined>{{ error }}</v-alert>
+      </v-col>
     </v-row>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 import serverRequestMixin from "@/mixins/serverRequest.mixin";
 import repairDialog from "./repairDialog";
 import repairList from "./repairList";
@@ -53,10 +62,15 @@ export default {
     repairList,
   },
 
+  computed: {
+    ...mapGetters(["user"]),
+  },
+
   data: () => ({
     loading: false,
     showRepairDialog: false,
     repairs: [],
+    error: "",
   }),
 
   mounted() {
@@ -65,6 +79,7 @@ export default {
 
   methods: {
     async getRepairs() {
+      this.error = "";
       this.loading = true;
       try {
         const response = await this.getRequest("/repairs");
@@ -72,7 +87,7 @@ export default {
         this.repairs = response.repairs;
       } catch (error) {
         this.loading = false;
-        this.deleteError = error.data.message;
+        this.error = error.data.message;
         if (error.status >= 500) console.error(error.data);
       }
     },
