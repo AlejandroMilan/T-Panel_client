e5a2fd27a42fcc40b0d92b4c150c229b3ad62b4b
Alejandro Milán <alex.13062001des@gmail.com>
2021-06-26 21:04:26 -0500

    PWA configured

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+  pwa: {
+    name: 'T-Panel',
+    themeColor: '#263238',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    }
+  }
 };
