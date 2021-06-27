6ac6b063d3edae1080b97cfb1b41d0a5f4703a2d
Alejandro Milán <alex.13062001des@gmail.com>
2021-06-26 21:21:39 -0500

    Service worker error fixed

---
+++ dev/sw.js
@@ -0,0 +1,10 @@
+export function postMessage(payload) {
+    if ('serviceWorker' in navigator && navigator.serviceWorker.controller) {
+        // check if it's properly installed
+        navigator.serviceWorker.controller.postMessage(payload);
+        return false;
+    } else {
+        // no service workers supported push the queue manualy
+        return true;
+    }
+}
