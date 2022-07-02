e89f643802b5223ff3031a6a9759b224c15e95ea
Alejandro Milán <alejandro.milan.serrano@gmail.com>
2022-07-02 05:23:55 -0500

    Inital dashboard loaded

---
+++ src/components/home/repairsCount.loading.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row>
+    <div class="d-md-flex">
+      <v-skeleton-loader
+        v-for="i in expectedLength"
+        :key="i"
+        type="image"
+        height="100"
+        :width="itemWidth"
+      ></v-skeleton-loader>
+    </div>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    expectedLength: { type: Number, default: 4 },
+  },
+
+  computed: {
+    isMobile() {
+      return !this.$vuetify.breakpoint.mdAndUp;
+    },
+
+    itemWidth() {
+      const { length } = this.expectedLength;
+      if (length && !this.isMobile) return `${100 / length}%`;
+      else return "100%";
+    },
+  },
+};
+</script>
--- src/components/home/welcomeCard.vue
+++ src/components/home/welcomeCard.vue
@@ -1,26 +1,65 @@
 <template>
-  <v-card tile>
+  <v-card :loading="loading" tile>
     <v-card-title class="secondary--text"
       >Buen día {{ user.name.split(" ")[0] }}</v-card-title
     >
     <v-card-text>
-      <update-promise></update-promise>
+      <loading-count v-if="loading" :expectedLength="5"></loading-count>
+      <div v-else class="d-md-flex">
+        <v-card
+          v-for="statusItem in repairsStatusCount"
+          :key="statusItem.status.key"
+          color="secondary"
+          dark
+          :width="statusItemWidth"
+          class="ma-1"
+        >
+          <v-card-title>
+            <span>{{ statusItem.count }}</span>
+            <v-spacer></v-spacer>
+            <v-chip
+              label
+              :color="getRepairStatusColor(statusItem.status.key)"
+              link
+              :to="{
+                name: 'Reparaciones',
+                query: { status: statusItem.status.key },
+              }"
+              >{{ statusItem.status.title }}</v-chip
+            >
+          </v-card-title>
+        </v-card>
+      </div>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-
-import updatePromise from "./updatePromise.vue";
+import repairsCountLoading from "./repairsCount.loading.vue";
 
 export default {
+  props: {
+    loading: { type: Boolean, default: false },
+    repairsStatusCount: { type: Array, default: () => [] },
+  },
+
   components: {
-    "update-promise": updatePromise,
+    "loading-count": repairsCountLoading,
   },
 
   computed: {
-    ...mapGetters(["user"]),
+    ...mapGetters(["user", "getRepairStatusColor"]),
+
+    isMobile() {
+      return !this.$vuetify.breakpoint.mdAndUp;
+    },
+
+    statusItemWidth() {
+      const { length } = this.repairsStatusCount;
+      if (length && !this.isMobile) return `${100 / length}%`;
+      else return "100%";
+    },
   },
 };
 </script>
